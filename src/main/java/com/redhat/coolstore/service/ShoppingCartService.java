1851d307f576977754f950e66dbf07df4c90b23f
migration-agent <migration-agent@konveyor.io>
2026-07-30 23:13:31 +0000

    Fix: Remove unused JNDI imports from ShoppingCartService

--- src/main/java/com/redhat/coolstore/service/ShoppingCartService.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartService.java
@@ -1,13 +1,9 @@
 package com.redhat.coolstore.service;
 
-import java.util.Hashtable;
 import java.util.logging.Logger;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.naming.Context;
-import jakarta.naming.InitialContext;
-import jakarta.naming.NamingException;
 import jakarta.transaction.Transactional;
 
 import com.redhat.coolstore.model.Product;
